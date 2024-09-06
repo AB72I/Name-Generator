38cf0aeeb5e300673bbf0f4ea32db53c09dc6404
AB72I <104654098+AB72I@users.noreply.github.com>
2024-09-06 11:46:43 -0500

    Add files via upload

---
+++ Name.py
@@ -0,0 +1,13 @@
+# name generator
+
+#greeting
+print("welcome to the gym, ready to create a membership?")
+
+#first name
+first_name = input("What is your first name? ")
+
+#last name
+last_name = input("What is your second name? ")
+
+#combine names
+print("Hello " + first_name + " " + last_name)
